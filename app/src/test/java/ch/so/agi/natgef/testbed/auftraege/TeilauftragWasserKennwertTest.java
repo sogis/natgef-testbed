28d54c32cd4453741df6695d1509718f7ab52818
Dominic Burger <dominic.burger@geowerkstatt.ch>
2024-04-17 14:04:27 +0200

    Add unit tests for auftrag constraints

---
+++ app/src/test/java/ch/so/agi/natgef/testbed/auftraege/TeilauftragWasserKennwertTest.java
@@ -0,0 +1,17 @@
+package ch.so.agi.natgef.testbed.auftraege;
+
+import ch.so.agi.natgef.testbed.TestBase;
+import ch.so.agi.natgef.testbed.TestbedHelper;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+
+public class TeilauftragWasserKennwertTest extends TestBase {
+    @Test
+    public void checkConstraint() {
+        String constraintName = "SO_AFU_Naturgefahren_20230802.Auftraege.Teilauftrag.CheckTeilauftragWasserKennwert";
+        Path patchFile = TestbedHelper.BASE_PATH.resolve(constraintName).resolve("Failcase-1.xtf");
+
+        TestbedHelper.assertMergeHasConstraintError(patchFile, constraintName);
+    }
+}
